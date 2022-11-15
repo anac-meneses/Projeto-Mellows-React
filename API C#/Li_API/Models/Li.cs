f48e9e65b36a10c998b616066fc6c306e17a18b7
carolcoser <carolinecoser@hotmail.com>
2022-11-15 16:04:56 -0300

    API + DB

---
+++ API C#/Li_API/Models/Li.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Li_API.Models
+{
+
+
+
+    [Table("Li")]
+    public class Li 
+
+    {
+        [Key]
+        public int LiId { get; set; }
+        [Required(ErrorMessage = "Informe o ID do Destino")]
+        [StringLength(50)]
+        public string Nivel { get; set; }
+        [Required(ErrorMessage = "Informe o Nivel")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe o Nome")]
+        public string Genero { get; set; }
+
+
+
+    }
+}
